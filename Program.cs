d6b9b5da13ac6101949bdeea3c1672749f3b017c
hip002205 <hip002205@hip-pro.info>
2024-04-02 14:12:16 +0900

    [Add] index

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample101
+{
+    class Program
+    {
+        /*
+         * HelloWorld
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld.");
+        }
+    }
+}
